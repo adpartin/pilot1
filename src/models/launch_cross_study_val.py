c18365562a203e1e88e522ea72e12ba9257bd252
Alex Partin <apartin@anl.gov>
2019-01-18 17:47:34 -0600

    Add main() to csv launch file.

--- src/models/launch_cross_study_val.py
+++ src/models/launch_cross_study_val.py
@@ -14,59 +14,61 @@ import pandas as pd
 import train_from_combined
 
 
-# Full set
-cross_study_sets = [
-    {'tr_src': ['ctrp'],
-     'te_src': ['ctrp', 'gdsc', 'ccle', 'gcsi']},
+def main(args):
 
-    {'tr_src': ['gdsc'],
-     'te_src': ['ctrp', 'gdsc', 'ccle', 'gcsi']},
+    # Full set
+    cross_study_sets = [
+        {'tr_src': ['ctrp'],
+        'te_src': ['ctrp', 'gdsc', 'ccle', 'gcsi']},
 
-    {'tr_src': ['ccle'],
-     'te_src': ['ctrp', 'gdsc', 'ccle', 'gcsi']},
+        {'tr_src': ['gdsc'],
+        'te_src': ['ctrp', 'gdsc', 'ccle', 'gcsi']},
 
-    {'tr_src': ['gcsi'],
-     'te_src': ['ctrp', 'gdsc', 'ccle', 'gcsi']},
-]
+        {'tr_src': ['ccle'],
+        'te_src': ['ctrp', 'gdsc', 'ccle', 'gcsi']},
 
-# Smaller set
-# cross_study_sets = [
-#     {'tr_src': ['ccle'],
-#      'te_src': ['ccle', 'gcsi']},
+        {'tr_src': ['gcsi'],
+        'te_src': ['ctrp', 'gdsc', 'ccle', 'gcsi']},
+    ]
 
-#     {'tr_src': ['gcsi'],
-#      'te_src': ['ccle', 'gcsi']},
-# ]
+    # Smaller set
+    # cross_study_sets = [
+    #     {'tr_src': ['ccle'],
+    #      'te_src': ['ccle', 'gcsi']},
 
-
-# Single run
-# idx = 2
-# train_from_combined.main(['-tr', *cross_study_sets[idx]['tr_src'],
-#                           '-te', *cross_study_sets[idx]['te_src']])
-
-# Multiple runs
-dfs = []
-for i in range(len(cross_study_sets)):
-    print('{} Run {} {}'.format('-'*30, i+1, '-'*30))
-    df_csv_scores, outdir = train_from_combined.main(
-        ['-tr', *cross_study_sets[i]['tr_src'],
-         '-te', *cross_study_sets[i]['te_src']])   
-    dfs.append(df_csv_scores)
+    #     {'tr_src': ['gcsi'],
+    #      'te_src': ['ccle', 'gcsi']},
+    # ]
 
 
-# Create csv table for each available metric 
-df = pd.concat(dfs, axis=0)
-dd = {}
-for m in df['metric'].unique():
-    csv = df[df['metric']==m].reset_index(drop=True)
-    csv.drop(columns=['metric'], inplace=True)
-    csv = csv.sort_values('train_src')
-    csv = csv[[csv.columns[0]] + sorted(csv.columns[1:])]
-    dd[m] = csv
+    # Single run
+    # idx = 2
+    # train_from_combined.main(['-tr', *cross_study_sets[idx]['tr_src'],
+    #                           '-te', *cross_study_sets[idx]['te_src']])
 
-    # save table
-    csv.to_csv(os.path.join(outdir, f'cross-study-val-{m}.csv'), index=False)
+    # Multiple runs
+    dfs = []
+    for i in range(len(cross_study_sets)):
+        print('{} Run {} {}'.format('-'*30, i+1, '-'*30))
+        df_csv_scores, outdir = train_from_combined.main(
+            ['-tr', *cross_study_sets[i]['tr_src'],
+             '-te', *cross_study_sets[i]['te_src'],
+             *args])   
+        dfs.append(df_csv_scores)
 
 
+    # Create csv table for each available metric 
+    df = pd.concat(dfs, axis=0)
+    for m in df['metric'].unique():
+        csv = df[df['metric']==m].reset_index(drop=True)
+        csv.drop(columns=['metric'], inplace=True)
+
+        # Sort rows and cols
+        csv = csv.sort_values('train_src')
+        csv = csv[[csv.columns[0]] + sorted(csv.columns[1:])]
+        
+        # save table
+        csv.to_csv(os.path.join(outdir, f'cross-study-val-{m}.csv'), index=False)
 
 
+main(sys.argv[1:])
