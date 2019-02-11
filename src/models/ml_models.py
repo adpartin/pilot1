817ec10b3e6fb7873ccca7ff66d8c411fd78a4cd
Alex Partin <apartin@anl.gov>
2019-02-10 21:10:15 -0600

    Minor change.

--- src/models/ml_models.py
+++ src/models/ml_models.py
@@ -33,6 +33,7 @@ except ImportError:
 
 
 def r2(y_true, y_pred):
+    from keras import backend as K
     SS_res =  K.sum(K.square(y_true - y_pred))
     SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
     return (1 - SS_res/(SS_tot + K.epsilon()))
